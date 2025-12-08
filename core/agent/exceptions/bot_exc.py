bc6090cc1d5c79632583ae9b0b885e2e5393c097
sharphu <stylite666@qq.com>
2025-12-08 20:00:53 +0800

    chore: agent project bot interface add tenant verification

--- core/agent/exceptions/bot_exc.py
+++ core/agent/exceptions/bot_exc.py
@@ -1,11 +1,11 @@
 from common.exceptions.base import BaseExc
-from agent.exceptions.codes import c_40600, c_40601, c_40602, c_40603
+from agent.exceptions.codes import c_40040, c_40600, c_40601, c_40602, c_40603
 
 
 class BotExc(BaseExc):
     pass
 
-
+AppAuthFailedExc = BotExc(*c_40040)
 BotProtocolSynchronizationFailedExc = BotExc(*c_40600)
 BotPublishFailedExc = BotExc(*c_40601)
 BotAuthFailedExc = BotExc(*c_40602)
